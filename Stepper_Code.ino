5d886b8067436c05e7882ec95152ddec3fb8c3e4
Hessa Albawardi <67501556+hessaalbawardi@users.noreply.github.com>
2020-06-30 23:35:08 +0300

    Add files via upload

---
+++ Stepper_Code.ino
@@ -0,0 +1,24 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 200;  
+
+// initializing the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+int stepCount = 0;  // number of steps the motor took 
+
+void setup() {
+}
+
+void loop() {
+  // read the sensor value:
+  int sensorReading = analogRead(A0);
+  // put the range from 0 to 100:
+  int motorSpeed = map(sensorReading, 0, 1023, 0, 250);
+  // the motor speed:
+  if (motorSpeed > 0) {
+    myStepper.setSpeed(motorSpeed);
+    // step 1/100 of a revolution:
+    myStepper.step(stepsPerRevolution / 100);
+  }
+}
